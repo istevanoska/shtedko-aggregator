cd9f2f4e4afae458d5578788c887892d76ca8947
istevanoska <ilinastevanoska@gmail.com>
2025-07-03 10:17:12 +0200

    Newest

--- DjangoProject3/settings.py
+++ DjangoProject3/settings.py
@@ -117,7 +117,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
-        'PASSWORD': 'P@ssw0rdilina123',
+        'PASSWORD': '',
         'OPTIONS': {
             'charset': 'utf8mb4',
             'use_unicode': True,
